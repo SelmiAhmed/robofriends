dfb5a085b42b0be7292f9064b8a9e42c81d83d2f
AhmedSelmi <ahmed_selmi@outlook.com>
2019-10-05 12:13:51 +0100

    commit

---
+++ robofriends/src/container/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import CardList from '../component/CardList';
+import SearchBox from '../component/SearchBox';
+import Scroll from '../component/Scroll';
+import './App.css';
+
+
+class App extends Component{
+	constructor(){
+		super()
+		this.state={
+			robots:[],
+	searchfield:''	
+		}
+	}
+
+componentDidMount(){
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response=> response.json())
+	.then(users=>this.setState({robots:users}))
+}
+
+
+
+
+
+
+
+
+
+
+	onSearchChange = (event) => {
+		this.setState({searchfield:event.target.value})
+
+
+	}
+render(){
+const filteredRobots=this.state.robots.filter(robots=>{
+return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+
+})
+if(this.state.robots.length===0)
+return <h1>Loading</h1>
+else{
+	return(
+ <div className='tc'>
+	<h1 className='f1'>RoboFriends</h1>
+	<SearchBox searchchange={this.onSearchChange}/>
+	<Scroll>
+    <CardList robots={filteredRobots}/></Scroll>
+ </div>
+
+
+
+);
+}}
+
+}
+export default App;
